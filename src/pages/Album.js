ee9a9a0afced1b4854ba91fe55aed2cdc852bc1e
Felipe Gama Danta <epilefgama2@gmail.com>
2022-03-07 19:03:12 -0300

    add pasta com pages

---
+++ src/pages/Album.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Album;
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default ProfileEdit;
