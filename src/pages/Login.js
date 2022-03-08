f257867c34add8d4f9ff362c3ea0fa833333437d
Felipe Gama Danta <epilefgama2@gmail.com>
2022-03-07 22:00:08 -0300

    inicio req 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,45 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loginInput: '',
+      isButtonDisable: true,
+      loading: false,
+      redirect: false
+    }
+  }
+
+  onInputChange = ({target}) => {
+    const { name, value } = target
+    this.setState({
+      [name]: value,
+    })
+  }
+
   render() {
+    const { loginInput, isButtonDisabled, loading, shouldRedirect } = this.state;
     return (
       <div data-testid="page-login">
-        <h1>Login</h1>
+        <label>
+          <h1>Login</h1>
+          <input
+            data-testid="login-name-input"
+            type="text"
+            name="loginInput"
+            onChange={this.onInputChange}
+            value={loginInput}
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          onClick={}
+        >
+          Entrar
+        </button>
       </div>
     );
   }
